6483d5782b64f3425a53ea215b8ba6a71833c190
Dario Pecelj <dariopecelj@Darios-MacBook-Pro.local>
2024-05-08 17:49:00 +0200

    lab 10

---
+++ labs/lab10/index.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+  $("#registration-form").validate({
+      rules: {
+          firstname: "required",
+          lastname: "required",
+          email: {
+              required: true,
+              email: true
+          },
+          password: {
+              required: true,
+              minlength: 8
+          }
+      },
+      messages: {
+          firstname: "Please enter your first name",
+          lastname: "Please enter your last name",
+          email: "Please enter a valid email address",
+          password: {
+              required: "Please provide a password",
+              minlength: "Your password must be at least 8 characters long"
+          }
+      },
+      submitHandler: function(form) {
+          form.submit();
+      }
+  });
+});
